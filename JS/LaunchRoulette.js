b987fa6f54aac077beef61a2ad5ef11c07e7941f
yhoo0094 <yhoo112@naver.com>
2022-09-26 22:45:44 +0900

    삭제 기능까지 구현 완료!

--- JS/LaunchRoulette.js
+++ JS/LaunchRoulette.js
@@ -11,6 +11,7 @@ window.onload = function(){
 	
 }
 
+//목록 추가 버튼
 function addItem(){
 	var text = document.getElementById("addItemInput").value
 	var colorCode = "#" + Math.round(Math.random() * 0xffffff).toString(16);
@@ -26,10 +27,24 @@ function addItem(){
 	    'animation'    : animation,          // Specify the animation to use.
 	    'pins'         : pins // Number of pins. They space evenly around the wheel.
 	});	
+	
+	document.getElementById("addItemInput").value = '';
 }
 
+//삭제 버튼
 function removeItem(me){
-	console.log($(me).html());
-	console.log($(me).attr('id'));
+	//console.log($(me).attr('id'));
+	segments.splice($(me).attr('id'),1);
+	numSegments = segments.length;
 	
+	theWheel = new Winwheel({
+	    'numSegments'  : numSegments,     // Specify number of segments.
+	    'outerRadius'  : 212,   // Set outer radius so wheel fits inside the background.
+	    'textFontSize' : 28,    // Set font size as desired.
+	    'segments'     : segments,       // Define segments including colour and text.
+	    'animation'    : animation,          // Specify the animation to use.
+	    'pins'         : pins // Number of pins. They space evenly around the wheel.
+	});	
+	
+	$(me).parent().parent().remove();
 }
